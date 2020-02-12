d406e181e6248917f98df26751ec6a40f1de14cc
William Alan Patterson <39136284+walanpat@users.noreply.github.com>
2020-02-12 11:16:18 -0500

    Base movement and camera code.
    pulled from many different tutorials, although mouselook.lock in center of screen isn't quite working on my machine.

---
+++ PlayerMovement.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float speed = 12f;
+    public float gravity = -9.81f;
+
+    public CharacterController controller;
+    public float groundDistance =0.4f;
+    public Transform groundCheck;
+    Vector3 velocity;
+    public LayerMask groundMask;
+    bool isGrounded;
+    public float jumpHeight = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+        if(isGrounded && velocity.y<0){
+            velocity.y = -2f;
+        }
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        
+        Vector3 move = transform.right * x + transform.forward * z;
+
+        if(Input.GetButtonDown("Jump") && isGrounded ){
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity );
+        }
+
+        controller.Move(move * speed * Time.deltaTime);
+    
+        velocity.y+= gravity * Time.deltaTime;
+        //Add Jump mechanic based on isGrounded;
+    // Debug.Log(isGrounded);
+        controller.Move(velocity * Time.deltaTime);
+
+    }
+}
